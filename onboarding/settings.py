bfb62b65f34f28cd216c392f01170a168b010264
yun <dbsdbf95@gmail.com>
2024-11-10 02:20:19 +0900

    static root 설정

--- onboarding/settings.py
+++ onboarding/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&f#5!jxjsz#p6l@_e3!2*^!0x45ogsc4l7qc^beze$2*ce2b6j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "43.202.56.198",
@@ -150,6 +150,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
