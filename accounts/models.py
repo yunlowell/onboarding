072cf4996c83d4135f81df58a7f67a701a9438ba
yun <dbsdbf95@gmail.com>
2024-11-09 21:49:25 +0900

    feat:username 공백 허용

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -14,3 +15,15 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+    # username 필드 수정
+    username_validator = RegexValidator(
+                regex=r'^[\w\s]+$',  # 공백 포함 유효성 검사
+                message="Username은 영어, 숫자, 공백만 허용됩니다."
+    )
+
+    username = models.CharField(
+        max_length=30,
+        unique=True,
+        validators=[username_validator]
+    )
